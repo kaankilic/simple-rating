4fd3126036720a861fa3cdcbbf944a1892e0f6ae
Kaan Kılıç <kaan@kaankilic.com>
2016-11-12 04:18:53 +0300

    init of component

---
+++ src/SimpleRating.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="ratings">
+    <div class="rating" v-for="n in max">
+      <i v-bind:class="icon" v-on:mouseover="star_over(n)" v-on:mouseout="star_out" v-on:click.stop.prevent="star_selected(n)" :class="{'active' : (overwritten_value >= n)}"></i>
+    </div>
+  <div class="search-input" v-if="!disabled">
+    <select v-bind:name="name">
+      <option v-for="n in max" v-bind:selected="overwritten_value==n" v-bind:value="n" v-text="n">1</option>
+    </select>
+  </div>
+  </div>
+</template>
+<style lang="scss">
+  .ratings{
+    display: block;
+    .rating{
+      display: inline-block;
+      .fa{
+        font-size: 18px;
+        color:#767676;
+        margin-right:5px;
+        opacity: 0.7;
+        cursor: pointer;
+      }
+      .fa-star.active{
+        color:#F1C40F;
+      }
+    }
+  }
+  .search-input {
+    select{
+      display:none;
+    }
+  }
+</style>
+<script>
+    export default {
+      data:function () {
+        return {
+          is_selected:true,
+          overwritten_value:this.value
+        }
+      },
+      props: {
+        max: Number,
+        value: Number,
+        disabled: Boolean,
+        name: String,
+        icon: {
+          "type":String,
+          "default": "fa fa-user"
+        }
+      },
+      ready:function() {
+        console.log('Component ready.')
+      },
+      watch: {
+        overwritten_value:function(){
+          this.selected_value = false;
+        }
+      },
+      methods: {
+        star_over: function(index){
+          if (this.disabled!=true) {
+            this.temp_value = this.overwritten_value;
+            this.overwritten_value = index;
+          }
+        },
+        star_out: function(){
+          if (this.disabled!=true && this.selected_value!=true) {
+            this.overwritten_value = this.temp_value;
+          }
+        },
+        star_selected: function(value){
+          if (this.disabled!=true) {
+            this.selected_value = true;
+            this.overwritten_value = value;
+          }
+        }
+      }
+    }
+</script>
